92c970d8d1cc2e98fabe5d1bfe78a11923a6e1ce
Andreas Dinter <andreas.dinter@systec-electronic.com>
2023-12-09 19:27:00 +0100

    Add custom machine 'ctr-700'
    Signed-off-by: Andreas Dinter <andreas.dinter@systec-electronic.com>

--- init_sd.sh
+++ init_sd.sh
@@ -9,7 +9,7 @@ fi
 
 DEV=$1
 
-xzcat ./build/arago-tmp-default-glibc/deploy/images/am62xx-evm/tisdk-base-image-am62xx-evm.wic.xz > "${DEV}"
+xzcat ./build/arago-tmp-default-glibc/deploy/images/ctr-700/tisdk-base-image-ctr-700.wic.xz > "${DEV}"
 
 sync
 sleep 1
