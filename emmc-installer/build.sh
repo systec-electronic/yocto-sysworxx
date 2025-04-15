4dd76bdd340e2ffd481d8e6d0fb8bc4c0f673fa3
Andreas Dinter <andreas.dinter@systec-electronic.com>
2025-04-15 16:55:08 +0200

    emmc-installer/build.sh: Adopt to new locations
    Signed-off-by: Andreas Dinter <andreas.dinter@systec-electronic.com>

--- emmc-installer/build.sh
+++ emmc-installer/build.sh
@@ -7,37 +7,38 @@
 #                                                                           #
 #  File:         build-installer.sh                                         #
 #  Description:  Create self extracting Yocto installer                     #
-#                including bringup.sh                                       #
 #                                                                           #
 #***************************************************************************#
 
 set -e
 
 script_dir=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
-deploy_dir="$script_dir/../build/deploy-ti/images/sysworxx"
+deploy_dir="${script_dir}/../build/deploy-ti/images/sysworxx"
 
 e_num_prefix="E004481"
 git_describe=$(git describe --tags)
 installer=${e_num_prefix}-sysworxx-image-default-emmc-installer-${git_describe}.sh
 
-sfx_header="./production-test/sfx-header"
+sfx_header="${script_dir}/sfx-header"
 tmp_tar_ball="$(mktemp -d)/install.tar.gz"
 
 echo "Build tarball with install files..."
 
-tar -hczvf "$tmp_tar_ball" \
-   -C "$deploy_dir" \
+rm -f ${installer}
+
+tar -hczvf "${tmp_tar_ball}" \
+   -C "${deploy_dir}" \
    tiboot3.bin \
    tispl.bin \
    u-boot.img \
    sysworxx-image-default-sysworxx.rootfs.tar.gz \
-   -C . \
+   -C "${script_dir}" \
    bringup.sh
 
-echo "Build sfx installer '$installer'..."
-echo cat $sfx_header "$tmp_tar_ball" >"$installer"
-cat $sfx_header "$tmp_tar_ball" >"$installer"
-chmod +x "$installer"
+echo "Build sfx installer '${installer}'..."
+echo cat ${sfx_header} "${tmp_tar_ball}" >"${installer}"
+cat ${sfx_header} "${tmp_tar_ball}" >"${installer}"
+chmod +x "${installer}"
 echo "done."
 
 rm -rv "$(dirname "$tmp_tar_ball")"
